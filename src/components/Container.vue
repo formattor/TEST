74518f52cbcecf06a71f3cdec6e3fac3f4fc42e0
zhangyushi <1193822368@qq.com>
2019-01-14 17:33:40 +0800

    new container&aside

---
+++ src/components/Container.vue
@@ -0,0 +1,59 @@
+<template>
+    <el-container>
+        <el-header>导航栏</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <Aside></Aside>
+                </el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+    </el-container>
+</template>
+
+<script>
+    //es5写法，引入侧边栏
+     export default {
+        components: {
+            Aside:require('./Aside.vue').default
+        },
+        methods: {},
+    }
+</script>
+
+
+<style>
+  .el-header, .el-footer {
+    background-color: #ffffcc;
+    color: #333;
+    text-align: left;;
+    line-height: 60px;
+    margin: 0;
+  }
+  
+  .el-aside {
+    background-color: #ffcccc;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #ccffff;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Container from '@/components/Container'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -7,9 +8,12 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: '/haha',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      component: Container
     }
   ]
 })
