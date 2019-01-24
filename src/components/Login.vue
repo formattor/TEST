fdca543e15ca6106931c5b2190208594b172dfb6
zhangyushi <1193822368@qq.com>
2019-01-24 15:05:11 +0800

    登陆窗口优化，主窗口优化

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="login">
-        用户名：
-        <el-input
-            placeholder="请输入邮箱或用户名"
-            v-model="input2">
-        </el-input><br>
-        密码：
-        <el-input
-            placeholder="请输入密码"
-            v-model="input21">
-        </el-input>
-        <el-button type="primary" plain @click="home">登陆</el-button>
+    <div class="bgimg">
+        <!-- <div class="bgimg">
+            <img src="../iamges/mountain house.jpg" alt="" 
+            height="1300px"
+            width="2823px">
+        </div> -->
+        <div class="login">
+            用户名：
+            <el-input
+                placeholder="请输入邮箱或用户名"
+                v-model="input2">
+            </el-input><br>
+            密码：
+            <el-input
+                placeholder="请输入密码"
+                v-model="input21">
+            </el-input>
+            <div style="margin:20px">
+            <el-button type="primary" style="display:block;margin:0 auto" plain @click="home">登陆</el-button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,7 +36,10 @@ export default {
           if(this.input2=='张与时'&&this.input21=='123'){
                 this.$router.push({path:'/home'})
           }else{
-              alert("账号或者密码错误")
+            this.$notify.error({
+            title: '错误',
+            message: '您输入错误'
+            });
           }
       }
   }
@@ -41,5 +53,18 @@ export default {
     width: 250px;
     padding: 30px;
     color: blueviolet;
+    margin:230px auto;
+    border-radius: 10px;
+    filter:alpha(Opacity=80);
+    -moz-opacity:0.9;
+    opacity: 0.9;
+}
+.bgimg{
+    position           : absolute;
+    width              : 100%;
+    height             : 100%;
+    background-image   : url("./../iamges/MHome.jpg");
+    background-size    : cover;
+    background-position: center;
 }
 </style>
